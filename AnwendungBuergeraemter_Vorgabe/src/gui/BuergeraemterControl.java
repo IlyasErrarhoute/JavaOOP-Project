519c90fee90dd98991b7c525ee5fc3149fd98068
Student <Student@PC-Praktikum-09>
2023-11-02 09:23:05 +0100

    commit two

---
+++ AnwendungBuergeraemter_Vorgabe/src/gui/BuergeraemterControl.java
@@ -0,0 +1,34 @@
+package gui;
+
+import java.io.IOException;
+
+import business.BuergeraemterModel;
+import javafx.stage.Stage;
+
+public class BuergeraemterControl {
+	private BuergeraemterView buergeraemterView;
+	private BuergeraemterModel buergeraemterModel;
+	public BuergeraemterControl(Stage stage) {
+		this.buergeraemterModel = new BuergeraemterModel();
+		this.buergeraemterView = new BuergeraemterView(stage , buergeraemterModel , this);
+		
+	}
+	void schreibeBuergeraemterInDatei(String typ) {
+		try {
+		if ("csv".equals(typ)) {
+			buergeraemterModel.schreibeBuergeraemterInCsvDatei();
+			buergeraemterView.zeigeInformationsfensterAn("saved ");
+			
+			
+		}
+		else {
+			buergeraemterView.zeigeFehlermeldungsfensterAn("noch nocht implementiert!");
+		}
+		}
+		catch (Exception e) {
+			 buergeraemterView.zeigeFehlermeldungsfensterAn("Unbekannter Fehler beim Speichern!");
+			// TODO: handle exception
+		}
+	}
+
+}
